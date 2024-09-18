0b7e45b15e2f050ff8a71262a7080a957d20e1da
Frandols <frandelosantoss12@gmail.com>
2024-09-18 10:02:56 -0300

    Cambia usuario y password a maestro

--- TP_Integrador/capa_logica/Autenticacion.vb
+++ TP_Integrador/capa_logica/Autenticacion.vb
@@ -1,19 +1,25 @@
 ﻿Public Class Autenticador
     Private usuarios As New Usuarios()
 
+    Public Sub New()
+        agregarUsuarios() ' Esto es temporal
+    End Sub
+
     Sub agregarUsuarios()
-        usuarios.agregar("admin", "admin")
+        usuarios.agregar("master", "master", "maestro")
 
         ' Esto es temporal
     End Sub
 
     Function autenticarUsuario(usuario As String, password As String) As Boolean
-        agregarUsuarios() ' Esto es temporal
-
         Return usuarios.verificarPassword(usuario, password)
     End Function
 
     Function obtenerUsuarioAutenticado(usuario As String, password As String) As Usuarios.Usuario
-
+        If autenticarUsuario(usuario, password) Then
+            Return usuarios.obtenerUno(usuario)
+        Else
+            Return Nothing
+        End If
     End Function
 End Class
