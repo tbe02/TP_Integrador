ecffcf2ac4b4a5764c5560c30d7f365587ce13e6
Frandols <frandelosantoss12@gmail.com>
2024-10-09 10:09:48 -0300

    Actualizar capa de datos

---
+++ TP_Integrador/capa_datos/Modelos.vb
@@ -0,0 +1,26 @@
+﻿Public Class Modelos
+    Private Shared instancia As Modelos = Nothing
+
+    Class Modelo
+        Public Property idModelo As Integer
+        Public Property nombre As String
+    End Class
+
+    Private modelos As List(Of Modelo)
+
+    Public Sub Agregar(modelo As Modelo)
+        modelos.Add(modelo)
+    End Sub
+
+    Public Function ObtenerTodos() As List(Of Modelo)
+        Return Me.modelos
+    End Function
+
+    Public Shared Function ObtenerInstancia() As Modelos
+        If instancia Is Nothing Then
+            instancia = New Modelos()
+        End If
+
+        Return instancia
+    End Function
+End Class
---
+++ TP_Integrador/capa_datos/TiposDeEquipo.vb
@@ -0,0 +1,48 @@
+﻿Public Class TiposDeEquipo
+    Private Shared instancia As TiposDeEquipo = Nothing
+
+    Class TipoDeEquipo
+        Public Property idTipoEquipo As Integer
+        Public Property nombre As String
+    End Class
+
+    Private tiposDeEquipo As List(Of TipoDeEquipo)
+
+    Public Sub Agregar(tipoDeEquipo As TipoDeEquipo)
+        tiposDeEquipo.Add(tipoDeEquipo)
+    End Sub
+
+    Public Sub New()
+        Agregar(New TipoDeEquipo With {
+            .idTipoEquipo = 1,
+            .nombre = "Computadora"
+        })
+
+        Agregar(New TipoDeEquipo With {
+            .idTipoEquipo = 2,
+            .nombre = "Impresora laser"
+        })
+
+        Agregar(New TipoDeEquipo With {
+            .idTipoEquipo = 3,
+            .nombre = "Impresora tinta"
+        })
+
+        Agregar(New TipoDeEquipo With {
+            .idTipoEquipo = 4,
+            .nombre = "Fotocopiadora"
+        })
+    End Sub
+
+    Public Function ObtenerTodos() As List(Of TipoDeEquipo)
+        Return Me.tiposDeEquipo
+    End Function
+
+    Private Shared Function ObtenerInstancia() As TiposDeEquipo
+        If instancia Is Nothing Then
+            instancia = New TiposDeEquipo()
+        End If
+
+        Return instancia
+    End Function
+End Class
