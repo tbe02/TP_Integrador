fd677c769e9d659419d1747726f6511ed7c83d22
Frandols <frandelosantoss12@gmail.com>
2024-10-30 10:08:59 -0300

    Cambiar string conexion a BDD

--- TP_Integrador/capa_datos/BaseDeDatos.vb
+++ TP_Integrador/capa_datos/BaseDeDatos.vb
@@ -1,6 +1,8 @@
 ﻿Public Class BaseDeDatos
     Public Function obtenerConexion() As SqlConnection
-        Dim conexion As New SqlConnection("server=THEO-NOTE\SQLEXPRESS; Initial Catalog=gestion_reparacion_equipos; Integrated Security=True")
+        Dim conexion As New SqlConnection("Server=192.168.200.49; Database=gestion_reparacion_equipos; User Id=sa; Password=DB_Password")
+
+        ' Fran MacBook: Server=192.168.200.49; Database=gestion_reparacion_equipos; User Id=sa; Password=DB_Password
 
         Return conexion
     End Function
