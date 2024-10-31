66285169a31de1bd4a34d307364fc8b1e4b7ec75
Francisco <frandelosantoss12@gmail.com>
2024-10-30 22:16:39 -0300

    Cambiar string conexion

--- TP_Integrador/capa_datos/BaseDeDatos.vb
+++ TP_Integrador/capa_datos/BaseDeDatos.vb
@@ -1,9 +1,10 @@
 ﻿Public Class BaseDeDatos
     Public Function obtenerConexion() As SqlConnection
-        Dim conexion As New SqlConnection("Server=THEO-NOTE\SQLEXPRESS; Initial catalog=gestion_reparacion_equipos; Integrated Security=True")
+        Dim conexion As New SqlConnection("Server=DESKTOP-HAOT1MM\SQLEXPRESS; Initial catalog=gestion_reparacion_equipos; Integrated Security=True")
 
         ' Fran MacBook: Server=192.168.200.49; Database=gestion_reparacion_equipos; User Id=sa; Password=DB_Password
-        ' Theo NOTEBOOK: Server=THEO-NOTE\SQLEXPRESS; Initial catalog=gestion_reparacion_equipos; Integrated Security=True 
+        ' Theo NOTEBOOK: Server=THEO-NOTE\SQLEXPRESS; Initial catalog=gestion_reparacion_equipos; Integrated Security=True
+        ' Fran Escritorio: Server=DESKTOP-HAOT1MM\SQLEXPRESS; Initial catalog=gestion_reparacion_equipos; Integrated Security=True
 
         Return conexion
     End Function
