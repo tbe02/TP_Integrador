6af1a3dd3fee13250903372a5c942ee37af5c803
Francisco <frandelosantoss12@gmail.com>
2024-11-14 23:50:53 -0300

    Cambiar bacpac

--- TP_Integrador/capa_datos/BaseDeDatos.vb
+++ TP_Integrador/capa_datos/BaseDeDatos.vb
@@ -1,6 +1,6 @@
 ﻿Public Class BaseDeDatos
     Public Function obtenerConexion() As SqlConnection
-        Dim conexion As New SqlConnection("Server=192.168.1.110; Database=gestion_reparacion_equipos; User Id=sa; Password=DB_Password")
+        Dim conexion As New SqlConnection("Server=DESKTOP-HAOT1MM\SQLEXPRESS; Initial catalog=gestion_reparacion_equipos; Integrated Security=True")
 
         ' Fran MacBook: Server=192.168.200.49; Database=gestion_reparacion_equipos; User Id=sa; Password=DB_Password
         ' Theo NOTEBOOK: Server=THEO-NOTE\SQLEXPRESS; Initial catalog=gestion_reparacion_equipos; Integrated Security=True
